dfceb41d850be453104beeb7a2ba17ee42b90802
Taher Hekmatfar <taher.hekmatfar@yahoo.com>
2015-06-04 09:29:38 +0430

    Amanati initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amanati::Application.config.secret_key_base = 'ecee99532feaf86dfd8b685dc239996e63a7fa893a3f6bed8a9859106e64bf3fcd597d1769b1f767d4a4cd958beba4caf3ccc4849048e85633075d8338042708'
