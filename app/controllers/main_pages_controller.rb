41383fd8c03ea8149827f253e7b8e806b6975425
vahid ranjbar <v.ranjbar68@gmail.com>
2015-06-08 15:32:33 +0430

    Added  main_pages controller and home.html.erb and about.html.erb

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,8 @@
+class MainPagesController < ApplicationController
+	layout false
+  def home
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Amanati::Application.routes.draw do
+  get "main_pages/home"
+  get "main_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
