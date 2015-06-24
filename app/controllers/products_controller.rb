8f22d552ccc8220b16b22207b96d5a98d341e47e
Ahmad Foroozani <ahmad121frzi@yahoo.com>
2015-06-24 17:15:34 +0430

    revised products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,18 @@
 class ProductsController < ApplicationController
   layout "main_layout"
   def index
-
+    if params[:query] == nil
+      @pro = Product.newest_first
+    else
+      @pro = Product
   end
 
   def show
-    @pro = Product.new
+    @pro = Product.find(params[:id])
   end
 
   def new
+    @pro = Product.new
   end
 
   def edit
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
 
-	default_scope -> { order(created_at: :desc) }
   	validates :user_id, presence: true
   	validates :category_id, presence: true
   	validates :description, presence: true
@@ -12,4 +11,14 @@ class Product < ActiveRecord::Base
   	validates :price, presence: true
   	validates :phone_number, presence: true, length: { maximum: 15 }
 
+   scope :search_city, lambda { |query| where(["products.city LIKE ?", "%#{query}%"] )}
+   scope :search_name, lambda { |query| where(["products.name LIKE ?",  "%#{query}%"] )}
+   scope :under_price, lambda { |query| where(["products.price <= ?", "#{query}"])}
+   scope :over_price, lambda { |query| where(["products.price >= ?", "#{query}"])}
+   scope :search_category, lambda { |query| where(["products.category_id = ?",  "%#{query}%"])}
+   scope :newest_first, lambda {order("products.created_at DESC")}
+   scope :oldest_first, lambda {order("products.created_at ASC")}
+   scope :lowest_price_first, lambda {order("products.price ASC")}
+   scope :highest_price_first, lambda{order("products.price DESC")}
+   
 end
