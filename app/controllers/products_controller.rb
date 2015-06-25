8877bea96e4b147be20be52cde62864678e3b2c3
Taher Hekmatfar <taher.hekmatfar@yahoo.com>
2015-06-25 15:40:22 +0430

    Item edit is added.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,13 @@
 class ProductsController < ApplicationController
   layout "main_layout"
-  $myCity =  ['اراک', 'اردبیل', 'اصفهان', 'اهواز', 'ایلام', 'بجنورد', 'بندرعباس', 'بوشهر', 'بیرجند', 'ارومیه', 'تبریز', 'تهران', 'خرم آباد', 'رشت', 'زاهدان', 'زنجان', 'ساری', 'سمنان', 'سنندج', 'شهرکرد', 'شیراز', 'قزوین', 'قم', 'کرج', 'کرمان', 'کرمانشاه', 'گرگان', 'مشهد', 'همدان', 'یاسوج', 'یزد']
-
+  $myCity =  ['تهران','اراک', 'اردبیل', 'اصفهان', 'اهواز', 'ایلام', 'بجنورد', 'بندرعباس', 'بوشهر', 'بیرجند', 'ارومیه', 'تبریز', 'خرم آباد', 'رشت', 'زاهدان', 'زنجان', 'ساری', 'سمنان', 'سنندج', 'شهرکرد', 'شیراز', 'قزوین', 'قم', 'کرج', 'کرمان', 'کرمانشاه', 'گرگان', 'مشهد', 'همدان', 'یاسوج', 'یزد']
+  $myCategory =  ['کتاب', 'تفریحی', 'لوازم شخصی', 'لوازم الکترونیکی', 'لوازم خانگی', 'املاک', 'خودرو']
   def index
 
   end
 
   def show
-    @pro = Product.new
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -15,12 +15,29 @@ class ProductsController < ApplicationController
   end
 
   def create
-    redirect_to(:controller => 'products', :action => 'new')
+    @product = Product.new(name: "کتاب رمان" , price: "2121", email:"aaa@yahoo.com")   
+      if @product.save
+        @temp = "salam"
+        flash[:success] = "آگهی مورد نظر ثبت گردید."
+        redirect_to @product
+      else
+        @temp = "bye"
+        render 'new'
+      end
   end
 
   def edit
+    @product = Product.new
+  end
+
+  def edit_choose
   end
 
   def delete
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :category_id, :user_id, :description, :price, :city, :address, :phone_number,:email, nil, :image, nil)
+    end
 end
